9db0866b16e3d7a935dbe4406245a76ca5f7450a
VeronicaVassallo <vvassallo94@gmail.com>
2024-06-17 15:29:22 +0200

    footer componet created + fix css menu opened

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './header/header.component';
 import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavComponent],
+  declarations: [AppComponent, HeaderComponent, NavComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
